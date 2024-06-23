9f75759195eb83913e357c79611c7da93f88436c
y_kawanishi <yat@yat-net.com>
2024-06-23 21:54:31 +0900

    save

--- salon-report/model/main-save-customer-fields.php
+++ salon-report/model/main-save-customer-fields.php
@@ -5,35 +5,37 @@
 function sr_save_customer_fields( $post_id ) {
 	global $post;
 
-	$prefix = SR_Config::PREFIX . 'report' . '0';
-	$names = [
-		'customer_visit_datetime',
-		'customer_visit_shop',
-		'customer_menu',
-		'customer_staff',
-		'designate',
-		'customer_peyment',
-		'customer_treatment_datail',
-		'customer_handdraw'
-	];
+	for ( $loop_count = 0; $loop_count < SR_Config::LOOP; $loop_count++ ) {
+		$prefix = SR_Config::PREFIX . 'report' . $loop_count;
+		$names = [
+			'customer_visit_datetime',
+			'customer_visit_shop',
+			'customer_menu',
+			'customer_staff',
+			'designate',
+			'customer_peyment',
+			'customer_treatment_datail',
+			'customer_handdraw'
+		];
 
-	if ( ! isset( $_POST[ SR_Config::PREFIX . 'report' ] ) ) {
-		return;
-	}
+		if ( ! isset( $_POST[ SR_Config::PREFIX . 'report' ] ) ) {
+			return;
+		}
 
-	if ( ! wp_verify_nonce( $_POST[ SR_Config::PREFIX . 'report' ], SR_Config::NAME . 'fields' ) ) {
-		return $post_id;
-	}
+		if ( ! wp_verify_nonce( $_POST[ SR_Config::PREFIX . 'report' ], SR_Config::NAME . 'fields' ) ) {
+			return $post_id;
+		}
 
-	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) { return $post_id; }
+		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) { return $post_id; }
 
-	if( ! current_user_can( 'edit_post', $post->ID ) ) { return $post_id; }
+		if( ! current_user_can( 'edit_post', $post->ID ) ) { return $post_id; }
 
-	foreach( $names as $name ) {
-		if ( ! empty( $_POST[ $name . '_' . $prefix ] ) ) {
-			update_post_meta( $post_id, $name . '_' . $prefix, $_POST[ $name . '_' . $prefix ] );
-		} else {
-			delete_post_meta( $post_id, $name . '_' . $prefix );
+		foreach( $names as $name ) {
+			if ( ! empty( $_POST[ $name . '_' . $prefix ] ) ) {
+				update_post_meta( $post_id, $name . '_' . $prefix, $_POST[ $name . '_' . $prefix ] );
+			} else {
+				delete_post_meta( $post_id, $name . '_' . $prefix );
+			}
 		}
 	}
 }
